1f7be5cce13ce98844f86e1894d8fefa88ba2809
Raymond Goo <raymond.goo124@gmail.com>
2025-05-27 13:38:12 +0800

    feat: Init repository.

---
+++ backend/app.js
@@ -0,0 +1,14 @@
+import express from "express"
+
+const app = express();
+
+app.use(express.static("dist"))
+
+
+
+app.listen(3000, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("Server started on port 3000");
+})
