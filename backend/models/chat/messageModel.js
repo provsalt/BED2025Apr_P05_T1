8fbbe4ff3b90637d00913fcba0544fe1c54f1022
Raymond Goo <raymond.goo124@gmail.com>
2025-06-22 18:09:40 +0800

    feat(chats): implement get message models

---
+++ backend/models/chat/messageModel.js
@@ -0,0 +1,28 @@
+import sql from "mssql";
+import {dbConfig} from "../../config/db.js";
+import {getChat} from "./chatModel.js";
+
+export const getMessages = async (userId, chatId) => {
+  const chat = await getChat(chatId)
+  if (!chat) {
+    return null
+  }
+
+  if (chat.message_sender !== userId || chat.message_receiver !== userId) {
+    return null
+  }
+
+  const db = await sql.connect(dbConfig);
+  const query = "SELECT * FROM ChatMsg WHERE chat_id = @chat_id";
+  const request = db.request();
+  request.input("id", userId);
+  const result = await request.query(query)
+
+  return result.recordset.length === 0 ? null : result.recordset
+}
+
+export const createMessage = async (userId, message, chatId) => {
+  const chat = await getChat(chatId)
+
+
+}
