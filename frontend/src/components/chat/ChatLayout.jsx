01287553f6a394e21ae53ce4ac2d7a99b989f2cd
Raymond Goo <raymond.goo124@gmail.com>
2025-06-27 17:40:52 +0800

    feat(chat): modify model to support last message.

--- frontend/src/components/chat/ChatLayout.jsx
+++ frontend/src/components/chat/ChatLayout.jsx
@@ -1,11 +1,13 @@
-import {Outlet} from "react-router";
+import {Outlet, useParams} from "react-router";
 import {ChatSideBar} from "@/components/chat/ChatSideBar.jsx";
+import useScreen from "@/hooks/useScreen.js";
 
 export const ChatLayout = () => {
-
+  const {isMobile} = useScreen();
+  const {chatId } = useParams();
   return (
     <div className="flex flex-1 pt-2">
-      <ChatSideBar />
+      {!isMobile && chatId && <ChatSideBar />}
       <Outlet />
     </div>
   )
--- frontend/src/components/chat/ChatList.jsx
+++ frontend/src/components/chat/ChatList.jsx
@@ -1,9 +1,10 @@
 import {Avatar} from "@/components/avatar/avatar.jsx";
 import {Link} from "react-router";
+import { formatDistanceToNow } from 'date-fns';
 
 export const ChatList = ({ conversations, selectedChatId, onSelectChat }) => {
   return (
-    <div className="w-full max-w-sm bg-gray-50 border-r p-4 space-y-2">
+    <div className="w-full max-w-sm bg-gray-50 space-y-2">
       <div className="flex flex-col gap-2">
         {conversations.map((chat) => (
           <Link to={`/chats/${chat.id}`} key={chat.id}>
@@ -18,6 +19,9 @@ export const ChatList = ({ conversations, selectedChatId, onSelectChat }) => {
                 <div className="font-semibold truncate">@{chat.username}</div>
                 <div className="text-gray-500 text-sm truncate">
                   {chat.lastMessage}
+                </div>
+                <div className="text-xs text-gray-400">
+                  {chat.lastMessageTime && formatDistanceToNow(new Date(chat.lastMessageTime), { addSuffix: true })}
                 </div>
               </div>
             </div>
