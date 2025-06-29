2170f13d3a73f5d8d495cf81ee55ebdb72982839
Raymond Goo <raymond.goo124@gmail.com>
2025-06-30 02:51:20 +0800

    fix(chat): commit socketprovider and essential websocket files.

---
+++ frontend/src/provider/SocketProvider.jsx
@@ -0,0 +1,32 @@
+import { createContext, useContext, useEffect, useState } from 'react';
+import io from 'socket.io-client';
+import { UserContext } from './UserContext';
+
+const SocketContext = createContext();
+
+export const useSocket = () => {
+  return useContext(SocketContext);
+};
+
+export const SocketProvider = ({ children }) => {
+  const [socket, setSocket] = useState(null);
+  const user  = useContext(UserContext);
+
+  useEffect(() => {
+    if (user && user.token && user.isAuthenticated) {
+      const newSocket = io(import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000', {
+        query: { token: user.token },
+      });
+
+      setSocket(newSocket);
+
+      return () => newSocket.close();
+    }
+  }, [user]);
+
+  return (
+    <SocketContext.Provider value={socket}>
+      {children}
+    </SocketContext.Provider>
+  );
+};
