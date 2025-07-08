f1f1c91bf0ac7e4e2278bc8204ce551371b17868
Pradyun Lakshmanan <pradsplat@gmail.com>
2025-07-08 16:23:31 +0800

    fix(nutrition): Update foodImageController.js to use random UUID for hashing

--- backend/controllers/nutrition/foodImageController.js
+++ backend/controllers/nutrition/foodImageController.js
@@ -1,7 +1,6 @@
-import crypto from "crypto";
+import { randomUUID } from "crypto";
 import { uploadFile, deleteFile } from "../../models/services/s3Service.js";
 
-// Example: POST /api/nutrition/upload-image
 export const uploadNutritionImage = async (req, res) => {
   const file = req.file;
 
@@ -10,8 +9,7 @@ export const uploadNutritionImage = async (req, res) => {
   }
 
   const ext = file.originalname.split('.').pop();
-  const fileHash = crypto.createHash('md5').update(file.buffer).digest('hex');
-  const filename = `${fileHash}.${ext}`;
+  const filename = `${randomUUID()}.${ext}`;
   const key = `nutrition-images/${filename}`;
 
   try {
