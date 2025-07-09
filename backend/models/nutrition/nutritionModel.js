60619386bf81652e1750fb3822ccb16da8335ccb
Pradyun Lakshmanan <pradsplat@gmail.com>
2025-07-09 19:48:15 +0800

    fix nutritionModel.js

--- backend/models/nutrition/nutritionModel.js
+++ backend/models/nutrition/nutritionModel.js
@@ -92,24 +92,3 @@ export const getMealsByUserId = async (userId) => {
     }
   }
 }
-
-async function getAllStudents() {
-  let connection;
-  try {
-    connection = await sql.connect(dbConfig);
-    const query = "SELECT student_id, name, address FROM Students";
-    const result = await connection.request().query(query);
-    return result.recordset;
-  } catch (error) {
-    console.error("Database error:", error);
-    throw error;
-  } finally {
-    if (connection) {
-      try {
-        await connection.close();
-      } catch (err) {
-        console.error("Error closing connection:", err);
-      }
-    }
-  }
-}
