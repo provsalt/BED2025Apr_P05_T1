ccd5ac4aff96eb731408225119e39404fe647027
Raymond Goo <raymond.goo124@gmail.com>
2025-07-26 03:03:16 +0800

    chore: parse does not have a success value.

--- backend/middleware/validateSchema.js
+++ backend/middleware/validateSchema.js
@@ -3,19 +3,9 @@ import { logInfo} from "../utils/logger.js";
 
 export function validateSchema(schema) {
   return (req, res, next) => {
-    const result = schema.parse(req.body);
-    if (!result.success) {
-      // this should never run because parse will throw an error if validation fails
-      const details = result.error.issues?.map(issue => ({
-        field: issue.path.join("."),
-        message: issue.message
-      }));
+    // throws a ZodError if validation fails. this will be caught by the global error handler
+    schema.parse(req.body);
 
-      return next(ErrorFactory.validation(
-        "Request body validation failed",
-        result.error
-      ));
-    }
     next();
   };
 }
