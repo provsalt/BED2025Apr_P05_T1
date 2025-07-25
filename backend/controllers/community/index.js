dc64185a0b070464484e941f61a11fb2c73933f7
-S <npshunxiang@gmail.com>
2025-07-25 23:47:52 +0800

    feat: reference getAllApprovedEvents (defined in separate branch)

--- backend/controllers/community/index.js
+++ backend/controllers/community/index.js
@@ -4,11 +4,11 @@ import { getUserMiddleware } from "../../middleware/getUser.js";
 import { validateSchema } from '../../middleware/validateSchema.js';
 import { CommunityInformation } from '../../utils/validation/community.js';
 import { genericUploadMiddleware } from '../../middleware/upload.js';
-import { getAllEvents } from './communityEventController.js';
+import { getApprovedEvents } from "./communityEventController.js";
 
 const router = express.Router();
 
 router.post("/create", getUserMiddleware, genericUploadMiddleware.single("image"), validateSchema(CommunityInformation), createEvent);
-router.get('/events', getAllEvents);
+router.get("/", getUserMiddleware, getApprovedEvents);
 
 export default router; 
