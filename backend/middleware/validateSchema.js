044feb7aacf454c10d25bcf6393a170f8c50f92e
Raymond Goo <raymond.goo124@gmail.com>
2025-07-26 15:20:23 +0800

    chore: simplify error handling in validateSchema.

--- backend/middleware/validateSchema.js
+++ backend/middleware/validateSchema.js
@@ -1,49 +1,38 @@
 import { ErrorFactory } from "../utils/AppError.js";
-import { logInfo} from "../utils/logger.js";
 
 export function validateSchema(schema) {
   return (req, res, next) => {
     // throws a ZodError if validation fails. this will be caught by the global error handler
-    schema.parse(req.body);
+    try {
+      schema.parse(req.body);
 
-    next();
+      next();
+    } catch (error) {
+      throw ErrorFactory.validation("Invalid body data", error)
+    }
   };
 }
 
 export function validateQuery(schema) {
   return (req, res, next) => {
-    const result = schema.safeParse(req.query);
-    if (!result.success) {
-      const details = result.error.issues?.map(issue => ({
-        field: issue.path.join("."),
-        message: issue.message
-      }));
-      
-      return next(ErrorFactory.validation(
-        "Query parameters validation failed",
-        details
-      ));
-    }
+    try {
+      schema.parse(req.query);
 
-    next();
+      next();
+    } catch (error) {
+      throw ErrorFactory.validation("invalid query parameters", error)
+    }
   };
 }
 
 export function validateParams(schema) {
   return (req, res, next) => {
-    const result = schema.safeParse(req.params);
-    if (!result.success) {
-      const details = result.error.issues?.map(issue => ({
-        field: issue.path.join("."),
-        message: issue.message
-      }));
-      
-      return next(ErrorFactory.validation(
-        "Path parameters validation failed",
-        details
-      ));
-    }
+    try {
+      schema.parse(req.query);
 
-    next();
+      next();
+    } catch (error) {
+      throw ErrorFactory.validation("invalid path parameters", error)
+    }
   };
 } 
