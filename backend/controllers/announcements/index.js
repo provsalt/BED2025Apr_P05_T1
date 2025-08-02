b5deff6110b30f4c37f8fd4d6464f3a70200d9d8
dylan <s10267635@connect.np.edu.sg>
2025-08-02 23:16:40 +0800

    fix: issues with the test checks

--- backend/controllers/announcements/index.js
+++ backend/controllers/announcements/index.js
@@ -1,6 +1,6 @@
 import {
   createAnnouncementController, deleteAnnouncementController, getAnnouncementByIdController,
-  getAnnouncementsController, dismissAnnouncementController
+  getAnnouncementsController
 } from "./announcementcontroller.js";
 import {Router} from "express";
 import {getUserMiddleware} from "../../middleware/getUser.js";
@@ -11,7 +11,6 @@ const router = Router();
 router.post("/", getUserMiddleware, authorizeRole(["Admin"]), createAnnouncementController);
 router.get("/", getAnnouncementsController); // Public route
 router.get("/user", getUserMiddleware); // User-specific announcements
-router.post("/:id/dismiss", getUserMiddleware, dismissAnnouncementController); // Dismiss announcement
 router.get("/:id", getAnnouncementByIdController); // Public route
 router.delete("/:id", getUserMiddleware, authorizeRole(["Admin"]), deleteAnnouncementController);
 
--- backend/tests/controllers/user/userController.test.js
+++ backend/tests/controllers/user/userController.test.js
@@ -460,7 +460,7 @@ describe('User Controller', () => {
 
       await uploadUserProfilePictureController(req, res, next);
 
-      expect(uploadFile).toHaveBeenCalledWith(req.file, 'uploads/mock-uuid');
+      expect(uploadFile).toHaveBeenCalledWith(req.file, 'uploads/mock-uuid', 1);
       expect(updateUserProfilePicture).toHaveBeenCalledWith(1, 'http://localhost:3000/api/s3?key=uploads/mock-uuid');
       expect(res.status).toHaveBeenCalledWith(200);
       expect(res.json).toHaveBeenCalledWith({
@@ -481,7 +481,7 @@ describe('User Controller', () => {
 
       await uploadUserProfilePictureController(req, res, next);
 
-      expect(deleteFile).toHaveBeenCalledWith('/uploads/old-uuid');
+      expect(deleteFile).toHaveBeenCalledWith('/uploads/old-uuid', 1);
     });
 
     it('should call next with error if there is a server error', async () => {
@@ -520,7 +520,7 @@ describe('User Controller', () => {
 
       await deleteUserProfilePictureController(req, res, next);
 
-      expect(deleteFile).toHaveBeenCalledWith('uploads/test-uuid');
+      expect(deleteFile).toHaveBeenCalledWith('uploads/test-uuid', 1);
       expect(updateUserProfilePicture).toHaveBeenCalledWith(1, null);
       expect(res.status).toHaveBeenCalledWith(200);
       expect(res.json).toHaveBeenCalledWith({ message: "Profile picture deleted successfully" });
